1c990f93e5d6d99f75d18546b1d8023907f06ff2
igorwanbarros <igorwanbarros@gmail.com>
2016-06-12 14:32:49 -0300

    Adding option to pass the class implementation FormatAccessorsAndMutators

--- src/CustomAccessorsAndMutators.php
+++ src/CustomAccessorsAndMutators.php
@@ -6,34 +6,26 @@ namespace Igorwanbarros\CustomAccessorAndMutator;
 trait CustomAccessorsAndMutators
 {
 
-    protected $customMutators = [];
-
-    protected $customAccessors = [];
-
-
     public function setAttribute($key, $value)
     {
-        if (array_key_exists($key, $this->customMutators) &&
-            isset($this->attributes[$key]) && !$this->hasSetMutator($key)
-        ) {
-            $method = $this->customMutators[$key];
-            if (method_exists($this, $method)) {
-                $this->attributes[$key] == $this->$method($value);
-                return;
+        if ($this->_hasExistsCustomMutators($key) && isset($this->attributes[$key]) && !$this->hasSetMutator($key)) {
+
+            if ($this->_hasMethodCustomAccessorsAndMutatorsExists($this->customMutators[$key])) {
+                $this->attributes[$key] = $this->_callMethodMutator($key, $value);
+                return $this;
             }
         }
 
-        parent::setAttribute($key, $value);
+        return parent::setAttribute($key, $value);
     }
 
 
     public function getAttributeValue($key)
     {
-        if (array_key_exists($key, $this->customAccessors) && !$this->hasGetMutator($key)) {
-            $method = $this->customAccessors[$key];
-            if (method_exists($this, $method))
-                return $this->$method($this->attributes[$key]);
-        }
+        if ($this->_hasExistsCustomAccessors($key) && !$this->hasGetMutator($key))
+            if ($this->_hasMethodCustomAccessorsAndMutatorsExists($this->customAccessors[$key]))
+                return $this->_callMethodAccessor($key, $this->attributes[$key]);
+
 
         return parent::getAttributeValue($key);
     }
@@ -44,10 +36,77 @@ trait CustomAccessorsAndMutators
         $attributes = parent::toArray();
         $attributesAccessors = [];
 
-        foreach ($this->customAccessors as $key => $methodAccessors)
-            if (method_exists($this, $methodAccessors) && !$this->hasGetMutator($key))
-                $attributesAccessors[$key] = $this->$methodAccessors($this->attributes[$key]);
+        if (!is_array($this->customAccessors))
+            return $attributes;
+
+        foreach ($this->customAccessors as $key => $classOrMethod) {
+            $value = $this->attributes[$key];
+
+            if (($this->_hasMethodCustomAccessorsAndMutatorsExists($classOrMethod) ||
+                    $this->_hasClassAccessorsAndMutatorsExists($classOrMethod)) &&
+                !$this->hasGetMutator($key)
+            )
+                $attributesAccessors[$key] = $this->_callMethodAccessor($key, $value);
+        }
 
         return array_merge($attributes, $attributesAccessors);
+    }
+
+
+    private function _hasExistsCustomAccessors($key)
+    {
+        return isset($this->customAccessors) && array_key_exists($key, $this->customAccessors);
+    }
+
+
+    private function _hasExistsCustomMutators($key)
+    {
+        return isset($this->customMutators) && array_key_exists($key, $this->customMutators);
+    }
+
+
+    private function _hasMethodCustomAccessorsAndMutatorsExists($method)
+    {
+        return method_exists($this, $method) || class_exists($method);
+    }
+
+
+    private function _callMethodAccessor($key, $value)
+    {
+        $classOrMethod = $this->customAccessors[$key];
+
+        if ($this->_hasClassAccessorsAndMutatorsExists($classOrMethod))
+            return $classOrMethod::get($value);
+
+        if ($this->_hasMethodCustomAccessorsAndMutatorsExists($classOrMethod))
+            return $this->{$classOrMethod}($value);
+
+        return $value;
+    }
+
+
+    private function _callMethodMutator($key, $value)
+    {
+        $classOrMethod = $this->customMutators[$key];
+
+        if ($this->_hasClassAccessorsAndMutatorsExists($classOrMethod)) {
+            return $classOrMethod::set($value);
+        }
+
+        if ($this->_hasMethodCustomAccessorsAndMutatorsExists($classOrMethod))
+            return $this->{$classOrMethod}($value);
+
+        return $value;
+    }
+
+
+    private function _hasClassAccessorsAndMutatorsExists($class)
+    {
+        if (!class_exists($class))
+            return false;
+
+        $reflection = new \ReflectionClass($class);
+
+        return $reflection->implementsInterface(FormatAccessorAndMutator::class);
     }
 }
---
+++ src/FormatAccessorAndMutator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Igorwanbarros\CustomAccessorAndMutator;
+
+
+interface FormatAccessorAndMutator
+{
+    //public static function get($value)
+    //{
+    //    return $value;
+    //}
+    //public static abstract function get($value);
+    public static function get($value);
+
+
+    //public static function set($value)
+    //{
+    //    return $value;
+    //}
+    //public static abstract function set($value);
+    public static function set($value);
+}
